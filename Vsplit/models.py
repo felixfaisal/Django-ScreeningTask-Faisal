5b7d8eaf59037997e45405cda772bfb335198ea1
felixfaisal <faisalahmedfarooq46@gmail.com>
2020-03-27 01:33:05 +0530

    Creating models and forms for uploading

--- Vsplit/models.py
+++ Vsplit/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class videos(models.Model):
+    Video = models.FileField(upload_to='uploaded')
+    Sub = models.FileField(upload_to='uploaded')
