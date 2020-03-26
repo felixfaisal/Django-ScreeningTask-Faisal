4f0753c36bb1a3f3818432a28d4c6dfb7bcc66ea
felixfaisal <faisalahmedfarooq46@gmail.com>
2020-03-27 02:20:11 +0530

    Completed Testing Video Analysis Functions

--- Vsplit/forms.py
+++ Vsplit/forms.py
@@ -1,7 +1,13 @@
 from django import forms
-from .models import videos
+from .models import videos,uploaded_audio
 
 class VideoForm(forms.ModelForm):
     class Meta:
         model=videos
         fields=('Video','Sub')
+
+class AudioForm(forms.ModelForm):
+    Audio = forms.FileField(widget=forms.FileInput, label='')
+    class Meta:
+        model=uploaded_audio
+        fields=('Audio',)
